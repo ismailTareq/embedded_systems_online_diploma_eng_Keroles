d0f044f76c5cb5b761128acb90e7319d53e78ede
ismailTareq <164344079+ismailTareq@users.noreply.github.com>
2024-05-12 12:53:47 +0300

    Delete 2.embedded_c/2.lect_two/LAB2 directory

--- 2.embedded_c/2.lect_two/LAB2/app.c
+++
@@ -1,6 +0,0 @@
-#include "uart.h"
-unsigned char string_buffer[100] = "learn-in-depth:ismail";
-void main(void)
-{
-	UART_SEND_STRING(string_buffer);
-}
--- 2.embedded_c/2.lect_two/LAB2/uart.h
+++
@@ -1,4 +0,0 @@
-#ifndef _UART_H_
-#define _UART_H_
-void UART_SEND_STRING(unsigned char* ptr_TX_string);
-#endif
