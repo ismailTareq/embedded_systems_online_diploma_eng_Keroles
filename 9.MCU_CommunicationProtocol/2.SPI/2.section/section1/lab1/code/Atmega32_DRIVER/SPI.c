65ca0b734b2df2d3d621ec463962a0aa60387305
ismailTareq <164344079+ismailTareq@users.noreply.github.com>
2024-08-06 02:19:46 +0300

    Add files via upload

---
+++ 9.MCU_CommunicationProtocol/2.SPI/2.section/section1/lab1/code/Atmega32_DRIVER/SPI.c
@@ -0,0 +1,158 @@
+/*
+ * SPI.c
+ *
+ * Created: 8/4/2024 3:15:21 PM
+ *  Author: ismail
+ */ 
+#include "SPI.h"
+SPI_Config_t *G_SPI_Config = NULL;	
+ReturnType SPI_Init(const SPI_Config_t* SPI_Config)
+{
+	ReturnType ret;
+	if (NULL == SPI_Config)
+	{
+		ret = E_NOT_OK;
+	} 
+	else
+	{
+		G_SPI_Config = SPI_Config;
+		
+		unsigned char SPCR_Init = 0x00; //0000 0000 use it to gather all of it and then SPCR = SPCR_Init
+		
+		SPCR_Init |= 1<<6;//enable SPI
+		
+		//set the master pins as output except MISO by writing
+			/*master		slave
+			*
+			*  OUTPUT       INPUT   MOSI
+			*  INPUT       OUTPUT   MISO
+			*  OUTPUT       INPUT   SCK
+			*  OUTPUT       INPUT   SS
+			*
+			*/ 
+		//Set Master or Slave
+		SPCR_Init |= SPI_Config->SPI_Mode;
+		
+		if (SPI_Config->SPI_Mode == SPI_MODE_MASTER)
+		{
+			SPI_DDR |=  ( (1 << 7) | (1 << 5) | (1 << 4) );
+			SPI_DDR &= ~(1 << 6);
+			SPCR_Init |= (1 << 4);
+			
+		} 
+		else if(SPI_Config->SPI_Mode == SPI_MODE_SLAVE)
+		{
+			SPI_DDR |= 1<<6;//output
+			
+			SPI_DDR &= ~((1<<7) | (1<<5) | (1<<4));//input
+		}
+			
+		//config the clock u choose 
+		// CPOL AND CPHA are related	//leading		//trailing
+		/*	0		 0				 sampling(rising)	setup(falling)	  mode0
+		 *	0		 1				 setup(rising)		sampling(falling) mode1
+		 *	1		 0				 sampling(falling)	setup(rising)	  mode2
+		 *	1		 1				 setup(falling)	sampling(rising)	  mode3	
+		*/
+		SPCR_Init |= SPI_Config->SPI_Polarity;
+		SPCR_Init |= SPI_Config->SPI_phase;
+			
+		//set the frequency of SCK rate
+		// SPI2X	SPR1	SPR2	SCK Frequency
+		/*	0		 0		 0		    f/4
+		 *	0		 0		 1			f/16
+		 *	0		 1		 0			f/64
+		 *	0		 1		 1			f/128
+		 *	1		 0		 0			f/2  double speed
+		 *	1		 0		 1			f/8  double speed
+		 *	1		 1		 0			f/32 double speed
+		 *	1		 1		 1			f/64 double speed
+		 */
+		if (SPI_Config->SPI_DoupleSpeed == CLK_SPEED_LOW)
+		{
+			SPCR_Init |= SPI_Config->SPI_Prescaler;
+		} 
+		else
+		{
+			SPCR_Init |= SPI_Config->SPI_Prescaler;
+			SPSR |= 1<<0;
+		}
+			
+		//set which one is gonna be sent first MSB or LSB 
+		/* DORD 
+		 *  0      MSB First
+		 *  1      LSB First
+		 */
+		SPCR_Init |= SPI_Config->SPI_DataOrder;
+		
+		//set the interrupt
+		if (SPI_Config->SPI_Interrupt == IRQ_ENABLE)
+		{
+			Enable_GI();
+			SPCR_Init |= SPI_Config->SPI_Interrupt;
+		} 
+		else
+		{
+			SPCR_Init |= SPI_Config->SPI_Interrupt;
+		}
+		
+		//init the SPCR Register
+		SPCR = SPCR_Init;
+		
+		ret = E_OK;
+	}
+	return ret;
+}
+
+ReturnType SPI_SendData(const SPI_Config_t* SPI_Config,unsigned char data)
+{
+	ReturnType ret ;
+	if (NULL == SPI_Config)
+	{
+		ret = E_NOT_OK;
+	}
+	else
+	{
+		SPDR = data;
+		while(!(SPSR & 1<<7));
+		ret = E_OK;
+	}
+	return ret;
+}
+
+ReturnType SPI_ReceiveData(const SPI_Config_t* SPI_Config,unsigned char *data)
+{
+	ReturnType ret ;
+	if (NULL == SPI_Config)
+	{
+		ret = E_NOT_OK;
+	}
+	else
+	{
+		while(!(SPSR & 1<<7));
+		*data = SPDR;
+		ret = E_OK;
+	}
+	return ret;
+}
+
+ReturnType SPI_SendString(const SPI_Config_t* SPI_Config,unsigned char *data)
+{
+	unsigned char i=0;
+	while (*data != '\0')
+	{
+		SPI_SendData(SPI_Config,data[i]);
+	}
+}
+
+ReturnType SPI_ReceiveString(const SPI_Config_t* SPI_Config,unsigned char *data)
+{
+	unsigned char i=0;
+	SPI_ReceiveData(SPI_Config,data[i]);
+	
+	while (*data != '\0')
+	{
+		i++;
+		SPI_ReceiveData(SPI_Config,data[i]);
+	}
+}
---
+++ 9.MCU_CommunicationProtocol/2.SPI/2.section/section1/lab1/code/Atmega32_DRIVER/main.c
@@ -0,0 +1,162 @@
+/*
+ * Atmega32_DRIVER.c
+ *
+ * Created: 7/4/2024 3:47:22 PM
+ * Author : ismail
+ */ 
+
+#include "DIO.h"
+#include "LCD.h"
+#include "SPI.h"
+#include "LED.h"
+#include "Push_Button.h"
+
+#define SS 4
+#define MOSI 5
+#define MISO 6
+#define SCK 7
+SPI_Config_t SPI;
+unsigned char ch;
+
+unsigned char data;
+int main(void)
+{
+	SPI.SPI_Mode = SPI_MODE_SLAVE;
+	SPI.SPI_DataOrder = FRAME_FORMAT_MSB_FIRST;
+	SPI.SPI_Prescaler = BAUDERATE_PRESCALER_16;
+	SPI.SPI_DoupleSpeed = CLK_SPEED_LOW;
+	SPI.SPI_phase = CLK_POLARITY_RISING_FALLING;
+	SPI.SPI_Polarity = CLK_PHASE_SAMPLE_SETUP;
+	SPI.SPI_Interrupt = IRQ_DISABLE;
+	SPI_Init(&SPI);
+	
+	DDRA = 0xFF;
+	while(1)
+	{
+		
+		SPI_ReceiveData(&SPI,&data);
+		PORTA = data;
+	}
+}
+/*int main(void)
+{
+SPI.SPI_Mode = SPI_MODE_MASTER;
+SPI.SPI_DataOrder = FRAME_FORMAT_MSB_FIRST;
+SPI.SPI_Prescaler = BAUDERATE_PRESCALER_16;
+SPI.SPI_DoupleSpeed = CLK_SPEED_LOW;
+SPI.SPI_phase = CLK_POLARITY_RISING_FALLING;
+SPI.SPI_Polarity = CLK_PHASE_SAMPLE_SETUP;
+SPI.SPI_Interrupt = IRQ_DISABLE;
+SPI_Init(&SPI);
+
+DDRA = 0xFF;
+for (ch = 0; ch <=255; ch++)
+{
+_delay_ms(1000);
+SPI_SendData(&SPI,ch);
+PORTA = ch;
+}
+}*/
+
+/*int main(void)
+{
+SPI.SPI_Mode = SPI_MODE_MASTER;
+SPI.SPI_DataOrder = FRAME_FORMAT_MSB_FIRST;
+SPI.SPI_Prescaler = BAUDERATE_PRESCALER_16;
+SPI.SPI_DoupleSpeed = CLK_SPEED_LOW;
+SPI.SPI_phase = CLK_POLARITY_RISING_FALLING;
+SPI.SPI_Polarity = CLK_PHASE_SAMPLE_SETUP;
+SPI.SPI_Interrupt = IRQ_DISABLE;
+SPI_Init(&SPI);
+DDRA = 0xFF;
+for(;ch<=255;ch++)
+{
+	_delay_ms(1000);
+SPI_SendData(&SPI,&ch);
+PORTA = ch;
+}
+}*/
+/*
+button_t btn_high = {
+	.button_pin.port = PORTA_INDEX,
+	.button_pin.pin = DIO_PIN0,
+	.button_pin.direction = DIO_DIRECTION_INPUT,
+	.button_pin.logic = DIO_LOW,
+	.button_connection = BUTTON_ACTIVE_HIGH,
+	.button_state = BUTTON_RELEASED
+};
+led_t led1 = {.port_name = PORTC_INDEX, .pin = DIO_PIN0, .led_status = DIO_LOW};
+
+SPI_Config_t SPI;
+button_state_t pressed = BUTTON_RELEASED;
+int main(void)
+{
+	button_initialize(&btn_high);
+	led_initialize(&led1);
+	//gpio_pin_direction_intialize(&(btn_high.button_pin));
+	
+	while (1)
+	{
+		button_read_state(&btn_high,&pressed);
+		if (pressed == BUTTON_PRESSED)
+		{
+			SPI_SendData(&SPI,BUTTON_PRESSED);
+		}
+		else
+		{
+			SPI_SendData(&SPI,BUTTON_RELEASED);
+		}
+	}
+}
+*/
+/*
+void SPI_MasterInit(void)
+{
+	SPI_DDR |= (1<<MOSI)|(1<<SCK)|(1<<SS);
+	SPI_DDR &= ~(1<<MISO);
+	SPCR = (1<<SPE)|(1<<MSTR)|(1<<SPR0);
+}
+uint8_t SPI_MasterTransmit(uint8_t data)
+{
+	SPDR = data;
+	while(!(SPSR & (1<<SPIF)));
+	return data;
+}
+int main(void)
+{
+	
+	SPI_MasterInit();
+	uint8_t ch = 0;
+	DDRA = 0xFF;
+	for(;ch<=255;ch++)
+	{
+		_delay_ms(1000);
+		PORTA = SPI_MasterTransmit(ch);
+	}
+}*/
+/*int main(void)
+{
+button_initialize(&btn_high);
+//led_initialize(&led1);
+SPI.SPI_Mode = SPI_MODE_MASTER;
+SPI.SPI_DataOrder = FRAME_FORMAT_MSB_FIRST;
+SPI.SPI_Prescaler = BAUDERATE_PRESCALER_16;
+SPI.SPI_DoupleSpeed = CLK_SPEED_LOW;
+SPI.SPI_phase = CLK_POLARITY_RISING_FALLING;
+SPI.SPI_Polarity = CLK_PHASE_SAMPLE_SETUP;
+SPI.SPI_Interrupt = IRQ_DISABLE;
+SPI_Init(&SPI);
+
+while (1)
+{
+button_read_state(&btn_high,&pressed);
+if (pressed == BUTTON_PRESSED)
+{
+SPI_SendData(&SPI,BUTTON_PRESSED);
+}
+else
+{
+SPI_SendData(&SPI,BUTTON_RELEASED);
+}
+}
+}*/
